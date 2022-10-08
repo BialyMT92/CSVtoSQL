1861fd0f5efd7a848ab2271bf64d7fb3f944d5e0
Bialek <Mateusz.Bialek@bshg.com>
2022-10-08 18:09:44 +0200

    Dodany config URL file

--- importtosql.py
+++ importtosql.py
@@ -1,7 +1,9 @@
-import shutil
-
 import pandas as pd
 from sqlalchemy import create_engine
+
+with open('config.txt') as f:
+    contents = f.read()
+    print(contents)
 
 #Sec
 svr_name = 'LDWSWISQLVAL01'
@@ -9,7 +11,7 @@ db_name = 'MaintenanceReport'
 u_name = 'PowerBI'
 u_pass = '111098tdg'
 
-df = pd.read_excel(r"\\ldwna001\Data\Factory Lodz\Departments\FDCL-PD\FDCL-PDT\7. MYSZKA KATARZYNA\05. PROJEKTY\Wizualizacja PowerBI\Awarie - dane STOEWA.xlsx")
+df = pd.read_excel(contents)
 
 df['Początek zakłócenia'] = df['Początek zakłócenia'].astype('string')
 df['Pocz. zakłóc. (godz.)'] = df['Pocz. zakłóc. (godz.)'].astype('string')
@@ -18,5 +20,5 @@ df['Koniec zakłóc.(godz.)'] = df['Koniec zakłóc.(godz.)'].astype('string')
 engine = create_engine("mssql+pyodbc://"+u_name+":"+u_pass+"@"+svr_name+"/MaintenanceReport?driver=SQL+Server", fast_executemany=True)
 engine.execute("delete from MaintenanceData")
 
-df.to_sql('MaintenanceData', if_exists='append', con=engine, index=False, chunksize=100000)
+df.to_sql('MaintenanceData', if_exists='append', con=engine, index=False)
 
