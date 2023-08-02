ffca2e6a84dbfa62d82a9ae328b49517fa5e34dd
BialyMT92 <mt.bialek92@gmail.com>
2023-08-02 14:28:37 +0200

    v1.0

--- CSVtoSQL.py
+++ CSVtoSQL.py
@@ -1,44 +1,56 @@
 import os.path
 import time
 import pandas as pd
-from pandas.io.parsers import read_csv
 from sqlalchemy import create_engine
-import pyodbc
-from sqlalchemy.engine import cursor
+import logging
 
-with open('config.txt') as f:
-    path = f.read()
-    print(path)
+configfile = pd.read_csv('config.txt', header=None).reset_index()
 
 desired_width=320
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns', 12)
-# Sec
+
+logging.basicConfig(filename='CSV_TO_SQL_LOGS.txt', filemode='a', level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+# Server
 svr_name = 'LDWSWISQLVAL01'
 db_name = 'MaintenanceReport'
 u_name = 'PowerBI'
 u_pass = '111098tdg'
 
-last_time = time.ctime(os.path.getmtime(path))
+engine = create_engine("mssql+pyodbc://" + u_name + ":" + u_pass + "@" + svr_name + "/" + db_name +
+                       "?driver=ODBC Driver 11 for SQL Server", fast_executemany=True)
 
+last_time = [0] * len(configfile)
 
 while True:
-    if time.ctime(os.path.getmtime(path)) != last_time:
-        rawCSV = pd.read_csv(path, sep=';')
+    for index, row in configfile.iterrows():
+        try:      
+            if time.ctime(os.path.getmtime(row[0])) != last_time[index]:
+                try:
+                    rawCSV = pd.read_csv(row[0], sep=';', on_bad_lines='skip')
+    
+                    rawCSV.drop(list(rawCSV.filter(regex='Unnamed')), inplace=True, axis=1)
+                    rawCSV = rawCSV[(rawCSV != rawCSV.columns).all(axis=1)]
+                    rawCSV.columns = rawCSV.columns.str.lower()
+                    rawCSV.columns = rawCSV.columns.str.strip()
+                    cols = pd.Series(rawCSV.columns)
+                    for dup in cols[cols.duplicated()].unique():
+                        cols[cols[cols == dup].index.values.tolist()] = [dup + '_' + str(i) if i != 0 else dup for i in
+                                                                         range(sum(cols == dup))]
+                    rawCSV.columns = cols
+                    rawCSV.to_sql(row[1], if_exists='replace', con=engine, index=False)
+                    last_time[index] = time.ctime(os.path.getmtime(row[0]))
+                    logger.info(f'New file "{row[0]}" send to SQL table "{row[1]}"!')
+                except:
+                    logger.error(f'Service was not able to send "{row[0]}" to SQL! Problem with the file.')
+                    last_time[index] = 0
+        except:
+            logger.error(f'File "{row[0]} not found!')
+            last_time[index] = 0
 
-        rawCSV.drop('Unnamed: 0', inplace=True, axis=1)
-        rawCSV.drop('Unnamed: 13', inplace=True, axis=1)
-
-        rawCSV = rawCSV.loc[(rawCSV["  Pocz.zakł."] != "  Pocz.zakł.")]
-
-        engine = create_engine("mssql+pyodbc://" + u_name + ":" + u_pass + "@" + svr_name + "/" + db_name +
-                               "?driver=ODBC Driver 11 for SQL Server", fast_executemany=True)
-        engine.execute("delete from MaintenanceData")
-        rawCSV.to_sql('MaintenanceData', if_exists='append', con=engine, index=False)
-        print('New file send to SQL!')
-        last_time = time.ctime(os.path.getmtime(path))
-        print(rawCSV)
-    time.sleep(5)
+    time.sleep(10)
     """ IF something 
             break
     """
--- importtosql_service.py
+++ importtosql_service.py
@@ -7,6 +7,7 @@ import os.path
 import time
 import pandas as pd
 from sqlalchemy import create_engine
+import logging
 
 
 class Pythonservice(win32serviceutil.ServiceFramework):
@@ -43,34 +44,51 @@ class Pythonservice(win32serviceutil.ServiceFramework):
 
     def main(self):
 
-        with open('D:\CopyToSQL\config.txt') as f:
-            path = f.read()
-            print(path)
+        configfile = pd.read_csv('D:\CopyToSQL\config.txt', header=None).reset_index()
 
-        # Sec
+        logging.basicConfig(filename='D:\CopyToSQL\CSV_TO_SQL_LOGS.txt', filemode='a', level=logging.INFO,
+                            format="%(asctime)-15s %(message)s")
+        logger = logging.getLogger()
+
+        # Server
         svr_name = 'LDWSWISQLVAL01'
         db_name = 'MaintenanceReport'
         u_name = 'PowerBI'
         u_pass = '111098tdg'
 
-        last_time = time.ctime(os.path.getmtime(path))
+        engine = create_engine("mssql+pyodbc://" + u_name + ":" + u_pass + "@" + svr_name + "/" + db_name +
+                               "?driver=ODBC Driver 11 for SQL Server", fast_executemany=True)
+
+        last_time = [0] * len(configfile)
 
         while True:
-            if time.ctime(os.path.getmtime(path)) != last_time:
-                rawCSV = pd.read_csv(path, sep=';')
+            for index, row in configfile.iterrows():
+                try:
+                    if time.ctime(os.path.getmtime(row[0])) != last_time[index]:
+                        try:
+                            rawCSV = pd.read_csv(row[0], sep=';', on_bad_lines='skip')
 
-                rawCSV.drop('Unnamed: 0', inplace=True, axis=1)
-                rawCSV.drop('Unnamed: 13', inplace=True, axis=1)
+                            rawCSV.drop(list(rawCSV.filter(regex='Unnamed')), inplace=True, axis=1)
+                            rawCSV = rawCSV[(rawCSV != rawCSV.columns).all(axis=1)]
+                            rawCSV.columns = rawCSV.columns.str.lower()
+                            rawCSV.columns = rawCSV.columns.str.strip()
+                            cols = pd.Series(rawCSV.columns)
+                            for dup in cols[cols.duplicated()].unique():
+                                cols[cols[cols == dup].index.values.tolist()] = [dup + '_' + str(i) if i != 0 else dup
+                                                                                 for i in
+                                                                                 range(sum(cols == dup))]
+                            rawCSV.columns = cols
+                            rawCSV.to_sql(row[1], if_exists='replace', con=engine, index=False)
+                            last_time[index] = time.ctime(os.path.getmtime(row[0]))
+                            logger.info(f'New file "{row[0]}" send to SQL table "{row[1]}"!')
+                        except:
+                            logger.error(f'Service was not able to send "{row[0]}" to SQL! Problem with the file.')
+                            last_time[index] = 0
+                except:
+                    logger.error(f'File "{row[0]} not found!')
+                    last_time[index] = 0
 
-                rawCSV = rawCSV.loc[(rawCSV["  Pocz.zakł."] != "  Pocz.zakł.")]
-
-                engine = create_engine("mssql+pyodbc://" + u_name + ":" + u_pass + "@" + svr_name + "/" + db_name +
-                                       "?driver=ODBC Driver 11 for SQL Server", encoding='utf8', fast_executemany=True)
-                engine.execute("delete from MaintenanceData")
-                rawCSV.to_sql('MaintenanceData', if_exists='append', con=engine, index=False)
-                print('New file send to SQL!')
-                last_time = time.ctime(os.path.getmtime(path))
-            time.sleep(5)
+            time.sleep(10)
             if win32event.WaitForSingleObject(self.hWaitStop, 5000) == win32event.WAIT_OBJECT_0:
                 break
             continue
